49096f0c565821221a68c8dbf053256ed9539779
Liang <yaoliang@gmail.com>
2016-08-17 00:10:40 +1000

    initial commit

---
+++ templates/vue_demo.js
@@ -0,0 +1,18 @@
+
+
+
+var vue = new Vue({
+    el:'#app',
+    data:{
+        event_place:'Melbourne Convention and Exhibition Centre',
+        event_intro:'德云社是中国首屈一指的相声团体，由郭德纲，于谦等200余位相声演员共同组成。在中国数个城市拥有分社及小剧场，同时在2012年正式于墨尔本成立首个德云社海外分社。 德云社相声2011年第一次海外巡演，第一站就是墨尔本，当年这一演出被誉为中国相声的海外破冰之旅，墨尔本也成了中国相声海外重镇。从2011年开始，每年德云社都会在澳洲做公开演出，今年是第五年，此次巡回，郭德纲及于谦将携其他五组德云社相声演员为大家呈现一场高水准的相声演出。',
+        event_seat_image:'http://d2o50i5c2dr30a.cloudfront.net/797de82d-7ae1-441c-a2b0-61a7a8747f51.jpg',
+        event_times:['2016年10月08日（周六） 20:10','2016年10月09日（周日） 20:10'],
+        is_event_time_selected:false,
+        event_time_selected:'',
+        seat_areas:['Area A','Area B','Area C','Area D','Area E'],
+        is_seat_area_selected:false,
+        seat_area_selected:'',
+    }
+    
+});
